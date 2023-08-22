2ec526db551f33e65b76a07170cbc268bba2f2dd
Yassin-ck <mohammedyassinck@gmail.com>
2023-08-22 16:09:32 +0530

    second commit

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 
 # Create your views here.
 
@@ -10,12 +10,14 @@ from django.http import HttpResponse
 def student_details(request,pk):
     stu = Student.objects.get(id=pk)
     serilizer = StudentSerializer(stu)   
-    json_data = JSONRenderer().render(serilizer.data)  
-    return HttpResponse(json_data , content_type='application/json')
+    # json_data = JSONRenderer().render(serilizer.data)  
+    # return HttpResponse(json_data , content_type='application/json')
+    return JsonResponse(serilizer.data)
 
 
 def student_list(request):
     stu = Student.objects.all()
     serilizer = StudentSerializer(stu,many=True)   
-    json_data = JSONRenderer().render(serilizer.data)  
-    return HttpResponse(json_data , content_type='application/json')
+    # json_data = JSONRenderer().render(serilizer.data)  
+    # return HttpResponse(json_data , content_type='application/json')
+    return JsonResponse(serilizer.data,safe=False)
