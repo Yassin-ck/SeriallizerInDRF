c0903666b83a32c58dd0bc2ebf201867e17b4eff
Yassin-ck <mohammedyassinck@gmail.com>
2023-08-22 15:58:04 +0530

    first commit

---
+++ api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('',views.student_list),
+    path('<int:pk>/',views.student_details)
+]   
---
+++ api/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def student_details(request,pk):
+    stu = Student.objects.get(id=pk)
+    serilizer = StudentSerializer(stu)   
+    json_data = JSONRenderer().render(serilizer.data)  
+    return HttpResponse(json_data , content_type='application/json')
+
+
+def student_list(request):
+    stu = Student.objects.all()
+    serilizer = StudentSerializer(stu,many=True)   
+    json_data = JSONRenderer().render(serilizer.data)  
+    return HttpResponse(json_data , content_type='application/json')
